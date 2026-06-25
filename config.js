127917a1c5fc002cf620b74999d92718405c0e63
kgweiyee-hub <kgweiyee@gmail.com>
2026-06-25 10:51:52 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-window.KG_CONFIG = {
-  SUPABASE_URL: "https://nsnbctfyuqokjnmcoxpb.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_IUJORXP6vPxX2l86VtuXqQ_Hh-53xak",
-  VIEW_PIN: "1234",
-  EDIT_PIN: "hengonghuat",
-  VIEW_EMAIL: "kg-view@kgplasterceil.local",
-  VIEW_PASSWORD: "kgview1234",
-  EDIT_EMAIL: "kg-edit@kgplasterceil.local",
-  EDIT_PASSWORD: "hengonghuat",
-  STORAGE_BUCKET: "kg-pass-license-files"
+window.KG_CONFIG={
+  SUPABASE_URL:"https://nsnbctfyuqokjnmcoxpb.supabase.co",
+  SUPABASE_ANON_KEY:"sb_publishable_IUJORXP6vPxX2l86VtuXqQ_Hh-53xak",
+  VIEW_PIN:"1234",
+  EDIT_PIN:"hengonghuat",
+  VIEW_EMAIL:"kg-view@kgplasterceil.local",
+  VIEW_PASSWORD:"kgview1234",
+  EDIT_EMAIL:"kg-edit@kgplasterceil.local",
+  EDIT_PASSWORD:"hengonghuat",
+  STORAGE_BUCKET:"kg-pass-license-files"
 };
